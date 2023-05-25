631e77139dcc46602f0ab5206cec6ca95c8954b3
semigrp <semigroupoid@gmail.com>
2023-05-25 10:29:25 +0900

    feat: support for osx and linux

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ extern crate chrono;
 extern crate clipboard;
 
 use chrono::prelude::*;
-use clipboard::x11_clipboard::X11ClipboardContext;
 use clipboard::ClipboardProvider;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 
+#[cfg(target_os = "macos")]
+type PlatformSpecificClipboardContext = clipboard::osx_clipboard::OSXClipboardContext;
+
+#[cfg(target_os = "linux")]
+type PlatformSpecificClipboardContext = clipboard::x11_clipboard::X11ClipboardContext;
+
 fn main() {
     let output_path = std::env::args().nth(1).unwrap();
     let mut file = OpenOptions::new().append(true).open(output_path).unwrap();
-    let mut clipboard: X11ClipboardContext = ClipboardProvider::new().unwrap();
+    let mut clipboard: PlatformSpecificClipboardContext = ClipboardProvider::new().unwrap();
     let mut old_clipboard_content = clipboard.get_contents().unwrap_or_else(|_| "".to_string());
 
     loop {
